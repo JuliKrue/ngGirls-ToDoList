3b69fa1a8981270018ae9e6954d110dc3cbd7162
JuliKrue <juli.krueger@outlook.de>
2022-05-10 11:09:34 +0200

    Added Hello world title on Input-Button-Unit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, InputButtonUnitComponent ],
-  declarations: [ AppComponent,InputButtonUnitComponent],
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent,InputButtonUnitComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: '
-  <p>
-  input-button-unit works!
-  </p>',
+  template: `
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+  `,
   styleUrls: ['./input-button-unit.component.css']
 })
+
 export class InputButtonUnitComponent implements OnInit {
-
-  title: string;
-
+  title = 'Hello World';
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
