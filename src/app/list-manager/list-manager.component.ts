9b5f5704d868cb457fbe9efc4e4fc372f25f5d21
JuliKrue <juli.krueger@outlook.de>
2022-07-07 10:33:00 +0200

    Feat(ToDoList): Added checkbox, remove button functionality and style

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-/**Interface wird importiert */
+import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
 import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
   template: `
-  <div class="todo-app">
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+    <div class="todo-app">
+      <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
 
-    <ul>
-      <li *ngFor="let todoItem of todoList">
-        <app-todo-item [item]="todoItem"></app-todo-item>
-      </li>
-    </ul>
+      <ul>
+        <li *ngFor="let todoItem of todoList">
+        <app-todo-item [item]="todoItem"
+        (remove)="removeItem($event)"
+        (update)="updateItem($event.item, $event.changes)"></app-todo-item>
+          </li>
+       
+      </ul>
     </div>
   `,
-  styleUrls: ['./list-manager.component.css']
+  styleUrls: ['./list-manager.component.scss']
 })
-
-
-/**addItem Methode wird implementiert, Info kommt von app-input-button-unit */
-  
 export class ListManagerComponent implements OnInit {
   todoList: TodoItem[];
 
@@ -33,5 +31,13 @@ export class ListManagerComponent implements OnInit {
 
   addItem(title: string): void {
     this.todoListService.addItem({ title });
+  }
+
+  removeItem(item): void {
+    this.todoListService.deleteItem(item);
+  }
+  
+  updateItem(item, changes): void {
+    this.todoListService.updateItem(item, changes);
   }
 }
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,23 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
-
 
 @Component({
   selector: 'app-todo-item',
   template: `
-  <div class="todo-item">
-    {{ item.title }}
+    <div class="todo-item">
+      <div>
+        <input type="checkbox"
+               class="todo-checkbox"
+               (click)="completeItem()"
+               [checked]="item.completed"/>
+        <span class="todo-title" [ngClass]="{'todo-complete': item.completed}">
+          {{ item.title }}
+        </span>
+      </div>
+
+      <button class="btn btn-red" (click)="removeItem()">
+        remove
+      </button>
     </div>
   `,
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
- /**Verweist auf verfügbare Objekte vom Typ TodoItem, um diese in der component Klasse und template nutzen zu können */
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeItem(): void {
+    this.remove.emit(this.item);
+  }
+
+  completeItem(): void {
+    this.update.emit({
+      item: this.item,
+      changes: { completed: !this.item.completed}
+    });
   }
 
 }
